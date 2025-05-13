8a75b43737b21eb43c4920c6bd69847ddd43583b
Matt <macavall@microsoft.com>
2025-05-13 12:36:10 -0500

    first

---
+++ src/main.c
@@ -0,0 +1,333 @@
+﻿/*******************************************************************************************
+*
+*   raylib [core] example - 3d camera first person
+*
+*   Example complexity rating: [★★☆☆] 2/4
+*
+*   Example originally created with raylib 1.3, last time updated with raylib 1.3
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2015-2025 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include "rcamera.h"
+
+#define MAX_COLUMNS 20
+
+void DrawUserPlane()
+{
+	DrawPlane((Vector3) { 0.0f, 0.0f, 0.0f }, (Vector2) { 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
+	DrawCube((Vector3) { -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
+	DrawCube((Vector3) { 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
+	DrawCube((Vector3) { 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
+}
+
+void HandleUserCamera(int *cameraMode, Camera *camera)
+{
+	// Update camera computes movement internally depending on the camera mode
+	// Some default standard keyboard/mouse inputs are hardcoded to simplify use
+	// For advanced camera controls, it's recommended to compute camera movement manually
+	UpdateCamera(camera, *cameraMode);                  // Update camera
+
+
+	// Camera PRO usage example (EXPERIMENTAL)
+	// This new camera function allows custom movement/rotation values to be directly provided
+	// as input parameters, with this approach, rcamera module is internally independent of raylib inputs
+	UpdateCameraPro(camera,
+		(Vector3) {
+		(IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) * 0.1f -      // Move forward-backward
+			(IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) * 0.1f,
+			(IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) * 0.1f -   // Move right-left
+			(IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) * 0.1f,
+			0.0f                                                // Move up-down
+	},
+		(Vector3) {
+		GetMouseDelta().x * 0.05f,                            // Rotation: yaw
+			GetMouseDelta().y * 0.05f,                            // Rotation: pitch
+			0.0f                                                // Rotation: roll
+	},
+		GetMouseWheelMove() * 2.0f);                              // Move to target (zoom)
+}
+
+void DrawScreenText(int *cameraMode, Camera *camera)
+{
+	// Draw info boxes
+	DrawRectangle(5, 5, 330, 100, Fade(SKYBLUE, 0.5f));
+	DrawRectangleLines(5, 5, 330, 100, BLUE);
+
+	DrawText("Camera controls:", 15, 15, 10, BLACK);
+	DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
+	DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
+	DrawText("- Camera mode keys: 1, 2, 3, 4", 15, 60, 10, BLACK);
+	DrawText("- Zoom keys: num-plus, num-minus or mouse scroll", 15, 75, 10, BLACK);
+	DrawText("- Camera projection key: P", 15, 90, 10, BLACK);
+
+	DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
+	DrawRectangleLines(600, 5, 195, 100, BLUE);
+
+	DrawText("Camera status:", 610, 15, 10, BLACK);
+	DrawText(TextFormat("- Mode: %s", (cameraMode == CAMERA_FREE) ? "FREE" :
+		(cameraMode == CAMERA_FIRST_PERSON) ? "FIRST_PERSON" :
+		(cameraMode == CAMERA_THIRD_PERSON) ? "THIRD_PERSON" :
+		(cameraMode == CAMERA_ORBITAL) ? "ORBITAL" : "CUSTOM"), 610, 30, 10, BLACK);
+	DrawText(TextFormat("- Projection: %s", (camera->projection == CAMERA_PERSPECTIVE) ? "PERSPECTIVE" :
+		(camera->projection == CAMERA_ORTHOGRAPHIC) ? "ORTHOGRAPHIC" : "CUSTOM"), 610, 45, 10, BLACK);
+	DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera->position.x, camera->position.y, camera->position.z), 610, 60, 10, BLACK);
+	DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera->target.x, camera->target.y, camera->target.z), 610, 75, 10, BLACK);
+	DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera->up.x, camera->up.y, camera->up.z), 610, 90, 10, BLACK);
+}
+
+void HandleIndex(int* index, Vector3* cylinderPos, int* cylinderDirection, float* cylinderSpeed)
+{
+	switch (*index)
+	{
+		case 0:
+			cylinderPos->x += *cylinderSpeed * *cylinderDirection;
+
+			if (cylinderPos->x > 10.0f || cylinderPos->x < -10.0f)
+			{
+				*cylinderDirection *= -1;  // Reverse direction
+			}
+			break;
+
+		case 1:
+			cylinderPos->y += *cylinderSpeed * *cylinderDirection;
+
+			if (cylinderPos->y > 10.0f || cylinderPos->y < -10.0f)
+			{
+				*cylinderDirection *= -1;  // Reverse direction
+			}
+			break;
+
+		case 2:
+			cylinderPos->z += *cylinderSpeed * *cylinderDirection;
+
+			if (cylinderPos->z > 10.0f || cylinderPos->z < -10.0f)
+			{
+				*cylinderDirection *= -1;  // Reverse direction
+			}
+			break;
+
+		default:
+			break;
+	}
+}
+
+void HandleBackspace(int* index)
+{
+	if (IsKeyPressed(KEY_BACKSPACE))
+	{
+		if (*index > 1)
+		{
+			*index = 0;
+		}
+		else
+		{
+			(*index)++;
+		}
+	}
+}
+
+void HandleCameraKeyPress(int* cameraMode, Camera* camera)
+{
+	// Update  
+	//----------------------------------------------------------------------------------  
+	// Switch camera mode  
+	if (IsKeyPressed(KEY_ONE))
+	{
+		*cameraMode = CAMERA_FREE;
+		camera->up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll  
+	}
+
+	if (IsKeyPressed(KEY_TWO))
+	{
+		*cameraMode = CAMERA_FIRST_PERSON;
+		camera->up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll  
+	}
+
+	if (IsKeyPressed(KEY_THREE))
+	{
+		*cameraMode = CAMERA_THIRD_PERSON;
+		camera->up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll  
+	}
+
+	if (IsKeyPressed(KEY_FOUR))
+	{
+		*cameraMode = CAMERA_ORBITAL;
+		camera->up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll  
+	}
+
+	// Switch camera projection  
+	if (IsKeyPressed(KEY_P))
+	{
+		if (camera->projection == CAMERA_PERSPECTIVE)
+		{
+			// Create isometric view  
+			*cameraMode = CAMERA_THIRD_PERSON;
+			// Note: The target distance is related to the render distance in the orthographic projection  
+			camera->position = (Vector3){ 0.0f, 2.0f, -100.0f };
+			camera->target = (Vector3){ 0.0f, 2.0f, 0.0f };
+			camera->up = (Vector3){ 0.0f, 1.0f, 0.0f };
+			camera->projection = CAMERA_ORTHOGRAPHIC;
+			camera->fovy = 20.0f; // near plane width in CAMERA_ORTHOGRAPHIC  
+			CameraYaw(camera, -135 * DEG2RAD, true);
+			CameraPitch(camera, -45 * DEG2RAD, true, true, false);
+		}
+		else if (camera->projection == CAMERA_ORTHOGRAPHIC)
+		{
+			// Reset to default view  
+			*cameraMode = CAMERA_THIRD_PERSON;
+			camera->position = (Vector3){ 0.0f, 2.0f, 10.0f };
+			camera->target = (Vector3){ 0.0f, 2.0f, 0.0f };
+			camera->up = (Vector3){ 0.0f, 1.0f, 0.0f };
+			camera->projection = CAMERA_PERSPECTIVE;
+			camera->fovy = 60.0f;
+		}
+	}
+}
+
+//void HandleIndex(int *index, Vector3 *cylinderPosX, Vector3 *cylinderPosY, Vector3* cylinderPosZ, int *cylinderDirection, int *cylinderSpeed)
+//{
+//	switch (*index)
+//	{
+//		case 0:
+//			cylinderPosX += *cylinderSpeed * *cylinderDirection;
+//
+//			if (cylinderPosX > 10.0f || cylinderPosX < -10.0f)
+//			{
+//				*cylinderDirection *= -1;  // Reverse direction
+//			}
+//			break;
+//
+//		case 1:
+//			cylinderPosY += *cylinderSpeed * *cylinderDirection;
+//
+//			if (cylinderPosY > 10.0f || cylinderPosY < -10.0f)
+//			{
+//				*cylinderDirection *= -1;  // Reverse direction
+//			}
+//			break;
+//
+//		case 2:
+//			cylinderPosZ += *cylinderSpeed * *cylinderDirection;
+//
+//			if (cylinderPosZ > 10.0f || cylinderPosZ < -10.0f)
+//			{
+//				*cylinderDirection *= -1;  // Reverse direction
+//			}
+//			break;
+//
+//		default:
+//			break;
+//	}
+//}
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+
+	int index = 0;
+
+	//float cylinderPosX = 0.0f;
+	//float cylinderPosY = 1.0f;
+	//float cylinderPosZ = -5.0f;
+	Vector3 cylinderPos = { 0.0f, 1.0f, -5.0f }; // Y is height from ground, Z to place in front of player
+	float cylinderSpeed = 0.05f;
+	int cylinderDirection = 1;
+
+	InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera first person");
+
+	// Define the camera to look into our 3d world (position, target, up vector)
+	Camera camera = { 0 };
+	camera.position = (Vector3){ 0.0f, 2.0f, 4.0f };    // Camera position
+	camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };      // Camera looking at point
+	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+	camera.fovy = 60.0f;                                // Camera field-of-view Y
+	camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+
+	int cameraMode = CAMERA_FIRST_PERSON;
+
+	// Generates some random columns
+	float heights[MAX_COLUMNS] = { 0 };
+	Vector3 positions[MAX_COLUMNS] = { 0 };
+	Color colors[MAX_COLUMNS] = { 0 };
+
+	for (int i = 0; i < MAX_COLUMNS; i++)
+	{
+		heights[i] = (float)GetRandomValue(1, 12);
+		positions[i] = (Vector3){ (float)GetRandomValue(-15, 15), heights[i] / 2.0f, (float)GetRandomValue(-15, 15) };
+		colors[i] = (Color){ GetRandomValue(20, 255), GetRandomValue(10, 55), 30, 255 };
+	}
+
+	DisableCursor();                    // Limit cursor to relative movement inside the window
+
+	SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+	//--------------------------------------------------------------------------------------
+
+	// Main game loop
+	while (!WindowShouldClose())        // Detect window close button or ESC key
+	{
+		HandleBackspace(&index); // Handle backspace key
+		HandleCameraKeyPress(&cameraMode, &camera); // Handle camera key press
+
+		HandleUserCamera(&cameraMode, &camera);
+
+		//---------------------------------- ------------------------------------------------
+
+		// Draw
+		//----------------------------------------------------------------------------------
+
+		HandleIndex(&index, &cylinderPos, &cylinderDirection, &cylinderSpeed);
+
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		BeginMode3D(camera);
+
+		DrawUserPlane();
+
+		// Vector3 cylinderPosition = { cylinderPosX, cylinderPosY, cylinderPosZ }; // Y is height from ground, Z to place in front of player
+		DrawCylinder(cylinderPos, 0.5f, 0.5f, 2.0f, 64, RED);
+		//DrawCylinderWires(cylinderPosition, 0.5f, 0.5f, 2.0f, 64, MAROON);
+
+		// Draw some cubes around
+		//for (int i = 0; i < MAX_COLUMNS; i++)
+		//{
+		//	DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
+		//	DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+		//}
+
+		// Draw player cube
+		if (cameraMode == CAMERA_THIRD_PERSON)
+		{
+			DrawCube(camera.target, 0.5f, 0.5f, 0.5f, PURPLE);
+			DrawCubeWires(camera.target, 0.5f, 0.5f, 0.5f, DARKPURPLE);
+		}
+
+		EndMode3D();
+
+		DrawScreenText(&cameraMode, &camera); // Draw info boxes
+
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
+
+	// De-Initialization
+	//--------------------------------------------------------------------------------------
+	CloseWindow();        // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
+
+	return 0;
+}
+
